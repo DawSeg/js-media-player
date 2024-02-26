efb78e98ea9877090f71e023018fb7f26995a018
DawSeg <buler43221@gmail.com>
2024-02-26 17:58:49 +0100

    addInitPages&ActivatePage

--- dist/js/App.js
+++ dist/js/App.js
@@ -2,12 +2,54 @@ import HomePage from './components/homePage.js';
 import { select, settings } from './settings.js';
 
 const app = {
+  initPages: function () {
+    const thisApp = this;
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+    const idFromhash = window.location.hash.replace('#/', '');
+    let pageMatchingHash = thisApp.pages[0].id;
+
+    for(let page of thisApp.pages) {
+      if(page.id == idFromhash) {
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+    
+    thisApp.activatePage(pageMatchingHash);
+
+    for (let link of thisApp.navLinks) {
+      link.addEventListener('click', function (event) {
+        const clickedElement = this;
+        event.preventDefault();
+        const id = clickedElement.getAttribute('href').replace('#', '');
+        thisApp.activatePage(id);
+        window.location.hash = '#/' + id;
+        //window.scrollTo(0, 0);
+      });
+    }
+    // window.addEventListener('scroll', function(event) {
+    //  event.preventDefault();
+    // }, { passive: false });
+  },
+
+  activatePage: function (pageId) {
+    const thisApp = this;
+    for (let page of thisApp.pages) {
+      page.classList.toggle(select.classNames.active, page.id == pageId);
+    }
+    for (let link of thisApp.navLinks) {
+      link.classList.toggle(select.classNames.active,
+        link.getAttribute('href') == '#' + pageId);
+    }
+  },
+
+
   initHome: function () {
     const thisApp = this;
     thisApp.homePage = document.querySelector(select.containerOf.homePage);
     new HomePage(thisApp.homePage);
   },
-
 
   initData: function () {
     const thisApp = this;
@@ -25,6 +67,7 @@ const app = {
     const thisApp = this;
     thisApp.initHome();
     thisApp.initData();
+    thisApp.initPages();
   }
 };
 
--- src/js/settings.js
+++ src/js/settings.js
@@ -1,15 +1,30 @@
 export const select = {
   templateOf: {
     homePage: '#template-home-page',
+    searchPage: '#template-search-page',
+    discoverPage: '#template-discover-page',
   },
   containerOf: {
-    homePage: '.home-page-wrapper'
+    homePage: '.home-page-wrapper',
+    searchPage: 'search-page-wrapper',
+    discover: 'discover-page-wrapper',
+    pages: '#pages',
+  },
+  classNames: {
+    active: 'active',
+  },
+  nav: {
+    links: '.nav-wrapper a'
   }
 };
 
 export const templates = {
+  searchPage: Handlebars.compile(
+    document.querySelector(select.templateOf.searchPage).innerHTML),
   homePage: Handlebars.compile(
     document.querySelector(select.templateOf.homePage).innerHTML),
+  discoverPage: Handlebars.compile(
+    document.querySelector(select.templateOf.discoverPage).innerHTML),
 };
 
 export const settings = {

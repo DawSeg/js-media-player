45e7b4a9f6ec7d92769cde5a3bbc23b95b09916f
DawSeg <buler43221@gmail.com>
2024-02-26 16:34:22 +0100

    addInitDataFromApi

--- dist/js/Settings.js
+++ dist/js/Settings.js
@@ -11,3 +11,11 @@ export const templates = {
   homePage: Handlebars.compile(
     document.querySelector(select.templateOf.homePage).innerHTML),
 };
+
+export const settings = {
+  db: {
+    url: '//' + window.location.hostname +
+      (window.location.hostname == 'localhost' ? ':3131' : ''),
+    songs: 'songs',
+  }
+};
--- src/js/App.js
+++ src/js/App.js
@@ -1,5 +1,5 @@
 import HomePage from './components/homePage.js';
-import { select } from './settings.js';
+import { select, settings } from './settings.js';
 
 const app = {
   initHome: function () {
@@ -8,9 +8,23 @@ const app = {
     new HomePage(thisApp.homePage);
   },
 
+
+  initData: function () {
+    const thisApp = this;
+    const url = settings.db.url + '/' + settings.db.songs;
+    thisApp.data = {};
+    fetch(url)
+      .then(rawResponse => rawResponse.json())
+      .then(parsedResponse => {
+        thisApp.data = parsedResponse;
+        console.log(parsedResponse);
+      });
+  },
+
   init: function () {
     const thisApp = this;
     thisApp.initHome();
+    thisApp.initData();
   }
 };
 
